bb4a733fd349a3594fcadfc14a692e1c45a32f69
Sanaz <snz5seifi@gmail.com>
2022-07-14 13:14:33 -0400

    Creating Rest Api

---
+++ src/main/java/com/example/getmesocialservice/resource/UserResource.java
@@ -0,0 +1,16 @@
+package com.example.getmesocialservice.resource;
+
+import com.example.getmesocialservice.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+    @GetMapping("/user")
+    public User getUser(){
+        User user = new User("Sanaz","Canada",22,"http://www.mypicurl");
+        return user;
+    }
+}
